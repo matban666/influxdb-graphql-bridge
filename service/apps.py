e1b7a30a93efe6e79c9c855e955a4e35836acf5f
matban666 <121500633+matban666@users.noreply.github.com>
2024-04-13 12:01:11 +0100

    Added docstring

--- service/apps.py
+++ service/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ServiceConfig(AppConfig):
+    """
+    Configuration class for the 'service' app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'service'
